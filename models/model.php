49040bb5257c3a97c0671806bdf5b2da7df08c23
unknown <prakhar.sharma2512@gmail.com>
2018-02-28 19:03:24 +0530

    28FEB 7:03

--- models/model.php
+++ models/model.php
@@ -14,11 +14,17 @@
         $data = array("code"=>"400","status"=>"Invalid email format");
         echo "<script>alert('Enter valid email address');</script>";
       }else{
-        $query = ($this->con->query("SELECT * from users where email='$email' AND password='$password'"));
+        $query = ($this->con->query("SELECT * from users where email='$email'"));
         if($query->num_rows){
-          $data = array("code"=>"200","status"=>"login successfully");
-          $_SESSION["email"] = $email;
-          return json_encode($data);
+          $this->$password = $password; // passsword from user while login
+          $query_r = mysqli_fetch_assoc($query);
+          $FromDb = $query_r['password']; // password from database
+          $isPasswordCorrect = password_verify($password, $FromDb);
+          if($isPasswordCorrect){
+            $_SESSION["email"] = $email;
+            $data = array("code"=>"200","status"=>"login successfully");
+            return json_encode($data);
+          }
         }else{
           $data = array("code"=>"400","status"=>"Email and password doesn't match");
           return json_encode($data);
@@ -38,7 +44,7 @@
           return json_encode($data);
         }else{
           $query2 = ($this->con->query("INSERT into users set first_name='$first_name',last_name='$last_name',email='$email',password='$password',mobile='$mobile'"));
-          if($query2->num_rows){  
+          if($query2){
             $data = array("code"=>"200","status"=>"User registered successfully");
             return json_encode($data);
           }else{
--- signup.php
+++ signup.php
@@ -5,11 +5,11 @@
     $first_name = $_REQUEST['first_name'];
     $last_name = $_REQUEST['last_name'];
     $email = $_REQUEST['email'];
-    $password = $_REQUEST['password'];
+    $password = password_hash($_REQUEST['password'], PASSWORD_DEFAULT);
     $mobile = $_REQUEST['mobile'];
     $controller -> signup_controller($first_name,$last_name,$email,$password,$mobile);
   }else{
-    $data = array("code"=>"400", "status"=>"Empty Fields");
+    $data = array("code"=>"400", "status"=>"Empty Field");
     echo json_encode($data);
   }
 ?>
